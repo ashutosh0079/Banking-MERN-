9540807a1e0c197365204397bb08ff03da9008a7
Ashutosh Gupta <ashu07988@gmail.com>
2024-06-28 13:52:27 +0530

    Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,34 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalancebox from '@/components/TotalBalancebox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {firstName:'Ashutosh',lastName:'Gupta',email:'ashu07988@gmail.com'};
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your account and transactions efficiently."
+                />
+            <TotalBalancebox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance ={1250}
+            />
+            </header>
+            recent transaction
+        </div>
+        <RightSidebar
+        user={loggedIn}
+        transactions = {[]}
+        banks={[{currentBalance:1356.90},{currentBalance:7000}]}
+        />
+    </section>
   )
 }
 
